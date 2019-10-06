bb8ebaa74141e9b9683afb8e49fac99cfdcb4c25
abdulrahmanFaid <abdulrahman_faid@outlook.com>
2019-10-06 18:42:47 +0200

    Setup all relations for listings

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -3,8 +3,90 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Listing extends Model
 {
+    use SoftDeletes;
+
     protected $guarded = [];
+
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public function scopeIsLive($query)
+    {
+        return $query->where('live', true);
+    }
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public function scopeIsNotLive($query)
+    {
+        return $query->where('live', false);
+    }
+    /**
+     * @return mixed
+     */
+    public function isLive()
+    {
+        return $this->live;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function cost()
+    {
+        return $this->category->price;
+    }
+
+    /**
+     * @param $query
+     * @param $category
+     * @return mixed
+     */
+    public function scopeFromCateory($query, $category)
+    {
+        return $this->where('category_id', $category->id);
+    }
+
+    /**
+     * @param $query
+     * @param $category
+     * @return mixed
+     */
+    public function scopeInArea($query, Area $area)
+    {
+        return $this->whereIn('area_id', array_merge(
+            [$area->id],
+            $area->descendants()->pluck('id')->toArray()
+        ));
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
 }
